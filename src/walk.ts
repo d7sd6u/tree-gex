4e3812fa497025a4924ddc9d9c6cabdaa1eb4852
d7sd6u <58745001+d7sd6u@users.noreply.github.com>
2025-06-18 15:00:43 +0400

    feat: add ignore walk node

--- src/walk.ts
+++ src/walk.ts
@@ -6,43 +6,73 @@ import { isObject } from './utils.js';
 export function walk(
   a: unknown,
   cb: (v: unknown) => void,
-  visited: Set<unknown> = new Set(),
+  ignore?: IgnorePredicate,
 ) {
-  if (visited.has(a)) return;
-  visited.add(a);
-  cb(a);
-  if (isObject(a)) {
-    if (Array.isArray(a)) {
-      return a.forEach((v) => walk(v, cb, visited));
+  const visited = new Set();
+  function recurse(a: unknown) {
+    if (visited.has(a)) return;
+    visited.add(a);
+    cb(a);
+    if (isObject(a)) {
+      if (Array.isArray(a)) {
+        for (const [idx, node] of a.entries()) {
+          if (!ignore?.(idx, a, node)) {
+            recurse(node);
+          }
+        }
+      } else {
+        for (const [key, node] of Object.entries(a)) {
+          if (!ignore?.(key, a, node)) {
+            recurse(node);
+          }
+        }
+      }
     }
-    return Object.values(a).forEach((v) => walk(v, cb, visited));
   }
+  recurse(a);
 }
 export function walkMatch(
   v: unknown,
   pattern: unknown,
   cb: (v: unknown, groups: CapturedGroups) => void,
+  ignore?: IgnorePredicate,
 ) {
-  walk(v, (node) => {
-    const [matched, groups] = matchAndCapture(node, pattern, undefined);
-    if (matched) {
-      cb(node, groups);
-    }
-  });
+  walk(
+    v,
+    (node) => {
+      const [matched, groups] = matchAndCapture(node, pattern, undefined);
+      if (matched) {
+        cb(node, groups);
+      }
+    },
+    ignore,
+  );
 }
+
+type IgnorePredicate = (
+  key: string | number | symbol,
+  parent: object | Array<unknown>,
+  node: unknown,
+) => boolean;
 
 export function accumWalkMatch<const P>(
   v: unknown,
   pattern: P,
+  ignore?: IgnorePredicate,
 ): { match: Resolve<P>; groups: ResolveGroups<P> }[] {
   const matches: { match: Resolve<P>; groups: ResolveGroups<P> }[] = [];
 
-  walkMatch(v, pattern, (node, groups) => {
-    matches.push({
-      match: node as Resolve<P>,
-      groups: groups as ResolveGroups<P>,
-    });
-  });
+  walkMatch(
+    v,
+    pattern,
+    (node, groups) => {
+      matches.push({
+        match: node as Resolve<P>,
+        groups: groups as ResolveGroups<P>,
+      });
+    },
+    ignore,
+  );
 
   return matches;
 }
